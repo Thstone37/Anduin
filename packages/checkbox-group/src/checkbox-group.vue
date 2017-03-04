16f1f51f55a25237d13279b15cf84e8f40d5096b
Thstone37 <82461760@qq.com>
2017-03-04 16:40:54 +0800

    fix checkbox

--- packages/checkbox-group/src/checkbox-group.vue
+++ packages/checkbox-group/src/checkbox-group.vue
@@ -7,6 +7,16 @@
 	export default{
 		name:"UiCheckboxGroup",
 
-		component:"UiCheckboxGroup"
+		component:"UiCheckboxGroup",
+
+		props:{
+			value:{}
+		},
+
+		watch:{
+			value(value){
+				this.$emit("change",value);
+			}
+		}
 	}
 </script>
--- packages/checkbox/src/checkbox.vue
+++ packages/checkbox/src/checkbox.vue
@@ -18,40 +18,65 @@
 	</label>
 </template>
 <script>
+    import emit from "../../../src/mixins/emit.js";
 	export default{
 		name:"UiCheckbox",
 
 		componentName:"UiCheckbox",
-
+        
+        mixins:[emit],
 		props:{
 			value:{},
 			label:{},
-			name:String
+			name:String,
+			trueLabel:[String,Number],
+			falseLabel:[String,Number]
 		},
 
 		data(){
 			return{
-				model:false
+				ownModel:false
 			}
 		},
 		computed:{
 			isGroup(){
 			  let parent=this.$parent;
-			  // while(parent){
-			  // 	if(parent.$options.componentName!="UiCheckBoxGroup")
-			  // }
+			  while(parent){
+			  	if(parent.$options.componentName!="UiCheckboxGroup"){
+			  		parent=parent.$parent;
+			  	}else{
+                   this._checkboxGroup=parent;
+                   return true;
+			  	}
+			  }
+			  return false;
 			},
 			isChecked(){
-              return this.model;
+              if({}.toString.call(this.model)==="[object Boolean]"){
+                   return this.model;
+              }else if(Array.isArray(this.model)){
+              	   return this.model.indexOf(this.label)>-1;
+              }else if(this.model!==null&&this.model!==undefined){
+                   return this.model===this.trueLabel;
+              }
+			},
+			store(){
+                return this.isGroup?this._checkboxGroup.value:this.value;
+			},
+			model:{
+				get(){
+                   return this.isGroup?this.store:this.value!==undefined?this.value:this.ownModel;
+				},
+				set(val){
+					if(this.isGroup){
+                       this.dispatch("UiCheckboxGroup","input",[val])
+					}else if(this.value!==undefined){
+                       this.emit("input",val);
+					}else{
+						this.ownModel=val;
+					}
+				}
 			}
-			// model:{
-			// 	get(){
-   //                 return this.value;
-			// 	},
-			// 	set(val){
-			// 		this.$emit("input",val)
-			// 	}
-			// }
 		}
 	}
 </script>
