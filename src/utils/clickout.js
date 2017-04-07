7aa818ab01882182372c9ee6067836bb80e04e64
Thstone37 <82461760@qq.com>
2017-04-07 19:35:32 +0800

    fix clickout directive

--- src/utils/clickout.js
+++ src/utils/clickout.js
@@ -2,24 +2,24 @@
 import Vue from "vue";
 import {on} from "./dom.js";
 
-const nodeList=[];
-const context="@@clickout"
+const nodeList = [];
+const context = "@@clickout"
 
-!Vue.prototype.$isServer&&on(document, "click", e =>{
+!Vue.prototype.$isServer && on(document, "click", e =>{
      nodeList.forEach(node =>node[context].documentHandler(e)
    ) 
 })
 
 export default{
 	bind(el,binding,vnode){
-        if(!vnode.context||
-        	el.contains(e.target)||
-        	(vnode.context.popperElement&&
+        if(!vnode.context ||
+        	el.contains(e.target) ||
+        	(vnode.context.popperElement &&
         	vnode.context.popperElement.contains(e.target))) return;
-        if(binding.expression&&
-        	el[context].methodName&&
+        if(binding.expression &&
+        	el[context].methodName &&
         	vnode.context[el[context].methodName]){
-
+        	vnode.context[el[context].methodName]()
         }
 	}
 }
