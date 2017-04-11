14e115c2ec98f19c71f94353971fdb63360f7915
Thstone37 <82461760@qq.com>
2017-04-12 00:20:02 +0800

    format vue-popper

--- src/utils/vue-popper.js
+++ src/utils/vue-popper.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-const PopperJs = Vue.prototype.$isServer?function(){}:require("./popper.es5.js");
+const PopperJs = Vue.prototype.$isServer ? function() {} : require("./popper.es5.js");
 export default {
-    
+
 }
