4179e78a54c6e43b7c3275a298610e4fe088919f
Thstone37 <82461760@qq.com>
2017-04-25 12:31:54 +0800

    fix resize-event

--- src/utils/resize-event.js
+++ src/utils/resize-event.js
@@ -1,4 +1,4 @@
-const isServer = typeof window === "undefined"
+const isServer = typeof window === "undefined";
 const requestAnimationFrame = (function() {
     if (isServer) return;
     var raf = window.requestAnimationFrame ||
@@ -23,7 +23,36 @@ const cancelAnimationFrame = (function() {
     }
 })();
 
-const resetTrigger=function(element){
-	const trigger=element._resetTrigger_;
-	
-}
+const resetTrigger = function(element) {
+    const trigger = element._resetTrigger_;
+    const expand = trigger.firstElementChild;
+    const contract = trigger.lastElementChild;
+    const expandChild = expand.firstElementChild;
+
+    contract.scrollLeft = contract.scrollWidth;
+    contract.scrollTop = contract.scrollHeight;
+    expandChild.style.width = expand.offsetWidth + 1 + "px";
+    expandChild.style.height = expand.offsetHeight + 1 + "px";
+    expand.scrollLeft = expand.scrollWidth;
+    expand.scrollTop = expand.scrollHeight;
+};
+
+const checkTrigger = function(element) {
+    return element.offsetWidth !== element._resizeLast_.width || element._resizeLast_.height;
+};
+const scrollListener = function(event) {
+    resetTrigger(this);
+    if (this._resizeRAF_) cancelAnimationFrame(this._resizeRAF_);
+    this._resizeRAF_ = requestAnimationFrame(() => {
+        if (checkTrigger(this)) {
+            this._resizeLast_.width = this.offsetWidth;
+            this._resizeLast_.Height = this.offsetHeight;
+            this._resizeListeners_.forEach((fn) => {
+                fn.call(this, event);
+            });
+        }
+    });
+};
+const attachEvent = isServer ? function() {} : document.attachEvent;
+const DOM_PREFIXES="webkit ms o moz".split(" ");
+const START_EVENTS="webkitAnimationStart animationStart "
