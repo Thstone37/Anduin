c3ae2e182b7c7def36a78c3bbb273c6b3af99824
Thstone37 <82461760@qq.com>
2017-05-09 15:43:07 +0800

    select vuei18n v5.0.3

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,25 +1,5 @@
-import VueI18n from "vue-i18n";
 import Vue from "vue";
-// import defaultMessage from "../locale/lang/zh-CN.js";
+import deepmerge from "deeepmerge";
+import Format from "./format";
+import defaultLang from "../locale/lang/zh-CN.js";
 
-Vue.use(VueI18n);
-let message = null;
-let locale=null;
-let i18n=null;
-const getLang = function(message) {
-    let lang = '';
-    Object.keys(message).forEach(key => {
-        lang = key;
-    })
-    return lang;
-}
-// let locale = getLang(message);
-
-const use = function(m) {
-    message = m || message;
-    locale=getLang(message);
-    var messageObj=require("../locale/lang/"+message+"-CN");
-    i18n=new VueI18n({locale: locale,messages: messageObj});
-    exports.i18n=i18n;
-}
-exports.use=use;
