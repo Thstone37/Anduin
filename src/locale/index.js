a5844d3b3ff07e5ed513f911babfaec08bc59fa2
Thstone37 <82461760@qq.com>
2017-05-12 18:11:10 +0800

    add i18n translate function t

--- src/locale/index.js
+++ src/locale/index.js
@@ -7,14 +7,27 @@ const format = Format(Vue);
 let lang = defaultLang;
 let merged = false;
 
-let i18nHandler=function(){
-	const vuei18n=Object.getPrototypeOf(this||Vue).$t;
-	if(typeof vuei18n=='function'){
-		if(!merged){
-			merged=true;
-			Vue.locale(Vue.config.lang,
-				deepmerge(lang,Vue.locale(Vue.config.lang)||{},{clone:true}))
-		}
-		return vuei18n.apply(this,arguments);
-	}
+let i18nHandler = function() {
+    const vuei18n = Object.getPrototypeOf(this || Vue).$t;
+    if (typeof vuei18n == 'function') {
+        if (!merged) {
+            merged = true;
+            Vue.locale(Vue.config.lang,
+                deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true }))
+        }
+        return vuei18n.apply(this, arguments);
+    }
 }
+
+export const t = function(path, options) {
+
+}
+
+export const use = function(l) {
+    lang = l || lang;
+}
+
+export const i18n = function(fn) {
+    i18nHandler = fn || i18nHandler;
+}
+export default { use, i18n, t }
