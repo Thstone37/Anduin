a60fb37fdf444e002492ea31b70fd7a007ea5901
Thstone37 <82461760@qq.com>
2017-05-12 18:06:16 +0800

    fix locale index.js

--- src/locale/index.js
+++ src/locale/index.js
@@ -3,6 +3,18 @@ import deepmerge from "deepmerge";
 import Format from "./format";
 import defaultLang from "../locale/lang/zh-CN.js";
 
-const format=Format(Vue);
-let lang=defaultLang;
-let merged=false;
+const format = Format(Vue);
+let lang = defaultLang;
+let merged = false;
+
+let i18nHandler=function(){
+	const vuei18n=Object.getPrototypeOf(this||Vue).$t;
+	if(typeof vuei18n=='function'){
+		if(!merged){
+			merged=true;
+			Vue.locale(Vue.config.lang,
+				deepmerge(lang,Vue.locale(Vue.config.lang)||{},{clone:true}))
+		}
+		return vuei18n.apply(this,arguments);
+	}
+}
