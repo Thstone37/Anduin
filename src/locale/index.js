d1ccdb9097591b12976cf1ec28bf7ab58c6ed29e
Thstone37 <82461760@qq.com>
2017-05-12 17:54:15 +0800

    fix locale index.js

--- src/locale/index.js
+++ src/locale/index.js
@@ -3,3 +3,6 @@ import deepmerge from "deepmerge";
 import Format from "./format";
 import defaultLang from "../locale/lang/zh-CN.js";
 
+const format=Format(Vue);
+let lang=defaultLang;
+let merged=false;
